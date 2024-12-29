064d9104461dde26f7e90e3d9a7f83dae58b6743
Rauda08 <raudajnnh08@gmail.com>
2024-12-29 17:34:32 +0700

    konfigurasi untuk app kontainer dan implementasi MahasiswaContainer

--- app/src/main/java/com/example/questapi/Repository/MahasiswaContainer.kt
+++ app/src/main/java/com/example/questapi/Repository/MahasiswaContainer.kt
@@ -1,2 +1,25 @@
 package com.example.questapi.Repository
 
+import com.example.questapi.service.MahasiswaService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+interface AppContainer {
+    val kontakRepository: MahasiswaRepository
+}
+
+class MahasiswaContainer : AppContainer {
+    private val baseUrl = "http://10.0.2.2:8080/umyTI/"
+    private val json = Json { ignoreUnknownKeys = true }
+    private val retrofit : Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl).build()
+
+    private val mahasiswaService: MahasiswaService by lazy {
+        retrofit.create(MahasiswaService::class.java) }
+
+    override val kontakRepository: MahasiswaRepository by lazy {
+        NetworkMahasiswaRepository(mahasiswaService) }
+}
