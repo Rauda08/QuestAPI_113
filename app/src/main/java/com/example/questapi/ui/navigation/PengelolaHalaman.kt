91cb838f4136ddc7b8e514574f40828a1d916cb5
Rauda08 <raudajnnh08@gmail.com>
2024-12-29 21:16:50 +0700

    membuat pengelola halaman untuk mengatur navigasi antara home EntryMhsScreen

---
+++ app/src/main/java/com/example/questapi/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,40 @@
+package com.example.questapi.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questapi.ui.view.DestinasiEntry
+import com.example.questapi.ui.view.DestinasiHome
+import com.example.questapi.ui.view.EntryMhsScreen
+import com.example.questapi.ui.view.HomeScreen
+
+
+@Composable
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                onDetailClick = { nim ->
+                    // Handle detail click
+                }
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntryMhsScreen(navigateBack = {
+                navController.navigate(DestinasiHome.route) {
+                    popUpTo(DestinasiHome.route) {
+                        inclusive = true
+                    }
+                }
+            })
+        }
+    }
+}
