28e80ffe2a28fe7d660ad32ad929a5dc8f4735d3
Rauda08 <raudajnnh08@gmail.com>
2024-12-29 20:57:07 +0700

    menambah fungsi utk menyimpan data di viewmodel

--- app/src/main/java/com/example/questapi/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/questapi/ui/viewmodel/InsertViewModel.kt
@@ -1,11 +1,13 @@
-package com.example.questapi.ui.theme.viewmodel
+package com.example.questapi.ui.viewmodel
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.questapi.Repository.MahasiswaRepository
 import com.example.questapi.model.Mahasiswa
+import kotlinx.coroutines.launch
 
 class InsertViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
     var uiState by mutableStateOf(InsertUiState())
@@ -14,7 +16,18 @@ class InsertViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
     fun updateInsertMhsState(insertUiEvent: InsertUiEvent) {
         uiState = InsertUiState(insertUiEvent = insertUiEvent)
     }
+
+    suspend fun insertMhs() {
+        viewModelScope.launch {
+            try {
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
 }
+
 
 data class InsertUiState(
     val insertUiEvent: InsertUiEvent = InsertUiEvent()
