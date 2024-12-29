9c3afc2def19c26c09099255817c2c2367ab0c25
Rauda08 <raudajnnh08@gmail.com>
2024-12-29 22:05:21 +0700

    membuat UpdateViewModel

---
+++ app/src/main/java/com/example/questapi/ui/viewmodel/UpdateViewModel.kt
@@ -0,0 +1,70 @@
+package com.example.questapi.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi.Repository.MahasiswaRepository
+import com.example.questapi.model.Mahasiswa
+import kotlinx.coroutines.launch
+
+class UpdateViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
+    var uiState by mutableStateOf(UpdateUiState())
+        private set
+
+    fun updateUiEvent(updateUiEvent: UpdateUiEvent) {
+        uiState = UpdateUiState(updateUiEvent = updateUiEvent)
+    }
+
+    fun loadMahasiswaData(nim: String) {
+        viewModelScope.launch {
+            try {
+                val mahasiswa = mhs.getMahasiswaById(nim)
+                uiState = UpdateUiState(updateUiEvent = mahasiswa.toUpdateUiEvent())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun updateMhs() {
+        viewModelScope.launch {
+            try {
+                mhs.updateMahasiswa(uiState.updateUiEvent.nim())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
+data class UpdateUiState(
+    val updateUiEvent: UpdateUiEvent = UpdateUiEvent()
+)
+
+data class UpdateUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
+fun UpdateUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jeniskelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
+fun Mahasiswa.toUpdateUiEvent(): UpdateUiEvent = UpdateUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jeniskelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
