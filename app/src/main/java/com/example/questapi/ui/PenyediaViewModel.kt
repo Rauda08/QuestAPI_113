0944ac89c9e619aef7c6d9144428931933eedc3d
Rauda08 <raudajnnh08@gmail.com>
2024-12-29 20:43:22 +0700

    membuat PenyediaViewModel

---
+++ app/src/main/java/com/example/questapi/ui/PenyediaViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.questapi.ui
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.questapi.MahasiswaApplications
+import com.example.questapi.ui.viewmodel.HomeViewModel
+import com.example.questapi.ui.viewmodel.InsertViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiKontak().container.kontakRepository) }
+        initializer { InsertViewModel(aplikasiKontak().container.kontakRepository) }
+    }
+}
+
+fun CreationExtras.aplikasiKontak(): MahasiswaApplications =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
