bb7dab57ffdcdebe2a26010301ce2d8a55d8e748
Rauda08 <raudajnnh08@gmail.com>
2024-12-29 17:29:39 +0700

    implementasi NetworkMahasiswaRepository dengan API

--- app/src/main/java/com/example/questapi/Repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/questapi/Repository/MahasiswaRepository.kt
@@ -12,5 +12,36 @@ interface MahasiswaRepository {
     suspend fun getMahasiswaById(nim: String): Mahasiswa
 }
 
+class NetworkMahasiswaRepository(
+    private val mahasiswaApiService: MahasiswaService
+) : MahasiswaRepository {
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        mahasiswaApiService.insertMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
+        mahasiswaApiService.updateMahasiswa(nim, mahasiswa)
+    }
+
+    override suspend fun deleteMahasiswa(nim: String) {
+        try {
+            val response = mahasiswaApiService.deleteMahasiswa(nim)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete mahasiswa. HTTP status code : ${response.code()} ")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getMahasiswa(): List<Mahasiswa> = mahasiswaApiService.getMahasiswa()
+    override suspend fun getMahasiswaById(nim: String): Mahasiswa {
+        return mahasiswaApiService.getMahasiswaById(nim)
+    }
+}
+
 
 
