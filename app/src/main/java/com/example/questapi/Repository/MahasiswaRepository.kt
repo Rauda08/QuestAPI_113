57cfbd27f15fa3e8e979f37ccc1d5f6c19545dae
Rauda08 <raudajnnh08@gmail.com>
2024-12-29 17:19:02 +0700

    tambah interface MahasiswaRepository untuk data mahasiswa

--- app/src/main/java/com/example/questapi/Repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/questapi/Repository/MahasiswaRepository.kt
@@ -1,2 +1,16 @@
 package com.example.questapi.Repository
 
+import com.example.questapi.model.Mahasiswa
+import com.example.questapi.service.MahasiswaService
+import java.io.IOException
+
+interface MahasiswaRepository {
+    suspend fun getMahasiswa(): List<Mahasiswa>
+    suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+    suspend fun updateMahasiswa (nim: String, mahasiswa: Mahasiswa)
+    suspend fun deleteMahasiswa (nim: String)
+    suspend fun getMahasiswaById(nim: String): Mahasiswa
+}
+
+
+
